78bd10a1f6080de4b73cea399175bb4455748867
Aaditya-Sgh <74530857+Aaditya-Sgh@users.noreply.github.com>
2024-02-07 14:16:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+ alert('I am so happy!');
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+ alert('I understand, everything will be okay.');
+});
